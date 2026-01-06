c3e9590ea27de972a0902c8e0fe77f69dc5de967
MUGILAN A <mugilanalagiri@MUGILANS-MacBook-Air.local>
2026-01-06 17:59:10 +0530

    Changed backend codes

--- backend/src/main/java/com/volunteer/portal/VolunteerPortalApplication.java
+++ backend/src/main/java/com/volunteer/portal/VolunteerPortalApplication.java
@@ -1,17 +1,12 @@
-
 package com.volunteer.portal;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VolunteerPortalApplication {
-    public static void main(String[] args) {
-        // Load environment variables from .env file
-        Dotenv dotenv = Dotenv.configure().load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
+    public static void main(String[] args) {
         SpringApplication.run(VolunteerPortalApplication.class, args);
     }
 }
